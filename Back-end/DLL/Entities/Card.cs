cbec1f92c6e2eb13b57b85a9588cd631ae37019b
vladiuslav <vladiuslav.vladiuslav1@gmail.com>
2022-06-11 18:42:21 +0200

    Added Entities

---
+++ Back-end/DLL/Entities/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL.Entities
+{
+    public class Card
+    {
+        public int CardId;
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int NumberOfLikes { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
---
+++ Back-end/DLL/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL.Entities
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
